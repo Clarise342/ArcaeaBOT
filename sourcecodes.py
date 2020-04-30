68fc3248576e23914fb39a77c2a4c6a1777be97d
Clarise342 <57488906+Clarise342@users.noreply.github.com>
2020-04-30 22:54:47 +0900

    Update sourcecodes.py

--- sourcecodes.py
+++ sourcecodes.py
@@ -1,4 +1,5 @@
 import discord
+import os
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix="sc/")
@@ -8,4 +9,4 @@ async def search(ctx, lang):
   if lang in ["py","js","rb","java","php","json"]:
     print("a")
 
-bot.run("")
+bot.run(os.environ.get('ENV_VAR_DISCORD_ID'))
