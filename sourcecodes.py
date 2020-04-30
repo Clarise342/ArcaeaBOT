307e436d03494da508a99fa5f0072b1abd1875d8
Clarise342 <57488906+Clarise342@users.noreply.github.com>
2020-04-30 22:28:56 +0900

    Update sourcecodes.py

--- sourcecodes.py
+++ sourcecodes.py
@@ -1,2 +1,9 @@
 import discord
 from discord.ext import commands
+
+bot = commands.Bot(command_prefix="sc/")
+
+@bot.command()
+async def search(ctx, lang):
+  if lang in ["py","js","rb","java","php","json"]:
+    print("a")
