f93738114d8de57c3d7d6a4634c7522162540a4a
Clarise342 <57488906+Clarise342@users.noreply.github.com>
2020-04-30 23:21:36 +0900

    Update sourcecodes.py

--- sourcecodes.py
+++ sourcecodes.py
@@ -1,11 +1,13 @@
 import discord
+import os
 from discord.ext import commands 
 
 bot = commands.Bot(command_prefix="sc/")
+token = os.environ['TOKEN']
 
 @bot.command()
 async def search(ctx, lang):
   if lang in ["py","js","rb","java","php","json"]:
     print("a")
 
-bot.run("TOKEN")
+bot.run(token)
