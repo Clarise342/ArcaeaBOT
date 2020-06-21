d92215ecee806e79fd537749bb8fc4668617c46c
Clarise342 <57488906+Clarise342@users.noreply.github.com>
2020-06-21 16:42:45 +0900

    Update arcaea.py

--- arcaea.py
+++ arcaea.py
@@ -274,7 +274,7 @@ async def on_ready():
                       
 @bot.event
 async def on_reaction_add(reaction, user):
-  print(reaction.emoji.id)
+  ch = bot.get_channel(723600070284673036)
+  await ch.send(reaction.emoji.id)
                   
-bot.add_listener(on_reaction_add)
 bot.run(token)
