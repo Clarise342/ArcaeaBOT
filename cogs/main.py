1be6bf993c2f42d0023c79d66f5f9ec9d9243b22
Clarice <57488906+Clarise342@users.noreply.github.com>
2020-09-18 21:55:58 +0900

    Update main.py

--- cogs/main.py
+++ cogs/main.py
@@ -1,8 +1,20 @@
 from discord.ext import commands
+from datetime import datetime
+import discord
 
 class Main(commands.Cog):
   
   def __init__(self, bot):
     self.bot = bot
+    
+  @self.bot.event
+  async def on_ready(self): 
+    embed = discord.Embed(title="<Ready> ArcaeaSupportBotが起動しました",color=0xBCF946) 
+    embed.timestamp = datetime.utcnow() 
+    embed.set_author(name="起動ログ",icon_url=self.bot.user.avatar_url) 
+    embed.set_footer(text="ver.2.0")
+    channel = bot.get_channel(723600070284673036) 
+    await channel.send(embed=embed) 
+    print("<Ready> 起動しました")
     
 def setup(bot): bot.add_cog(Main(bot))
