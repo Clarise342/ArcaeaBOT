15c0ef1278c4f8f5ce55d72968ba53020761cf06
Clarice <57488906+Clarise342@users.noreply.github.com>
2020-09-21 14:27:34 +0900

    Update main.py

--- cogs/main.py
+++ cogs/main.py
@@ -2,9 +2,6 @@ from discord.ext import commands
 from datetime import datetime
 import discord, json
 
-with open("guild.json", "r", encoding="utf-8") as f:
-  guilddata = json.load(f)
-
 class Main(commands.Cog):
   
   def __init__(self, bot):
@@ -13,18 +10,33 @@ class Main(commands.Cog):
     
   @commands.command()
   async def help(self, ctx):
-    await ctx.message.edit(after_delete=3.0) 
-    embed = discord.Embed(title="'a;<コマンド名>'で使用できます",color=0x74596d) 
-    embed.timestamp = datetime.utcnow() 
-    embed.add_field(name="help",value="ヘルプを表示します",inline=False) 
-    embed.add_field(name="sinfo <曲名>",value="楽曲の情報を表示します",inline=False) 
-    embed.add_field(name="pinfo <パートナー名>",value="パートナーの情報を表示します",inline=False) 
-    embed.add_field(name="acsong (回数)",value="ランダムに楽曲を選びます",inline=False) 
-    embed.add_field(name="acpartner",value="ランダムにパートナーを選びます",inline=False) 
-    embed.add_field(name="set",value="設定パネルを表示します",inline=False) 
-    embed.add_field(name="Other",value="BOTの導入は[こちら](https://discord.com/api/oauth2/authorize?client_id=702587324718120991&permissions=60480&scope=bot)",inline=False) 
-    embed.set_author(name="ヘルプ",icon_url=self.bot.user.avatar_url) 
-    embed.set_footer(text=f"送信者 : {ctx.author.name}") 
+    await ctx.message.edit(after_delete=3.0)
+    with open("guild.json", "r", encoding="utf-8") as f:
+      guilddata = json.load(f)
+    if guilddata[str(ctx.guild.id)]["lang"] == "jpn":
+      embed = discord.Embed(title="'a;<コマンド名>'で使用できます",color=0x74596d) 
+      embed.timestamp = datetime.utcnow() 
+      embed.add_field(name="help",value="ヘルプを表示します",inline=False) 
+      embed.add_field(name="sinfo <曲名>",value="楽曲の情報を表示します",inline=False) 
+      embed.add_field(name="pinfo <パートナー名>",value="パートナーの情報を表示します",inline=False) 
+      embed.add_field(name="acsong (回数)",value="ランダムに楽曲を選びます",inline=False) 
+      embed.add_field(name="acpartner",value="ランダムにパートナーを選びます",inline=False) 
+      embed.add_field(name="set",value="設定パネルを表示します",inline=False) 
+      embed.add_field(name="Other",value="BOTの導入は[こちら](https://discord.com/api/oauth2/authorize?client_id=702587324718120991&permissions=60480&scope=bot)",inline=False) 
+      embed.set_author(name="ヘルプ",icon_url=self.bot.user.avatar_url) 
+      embed.set_footer(text=f"送信者 : {ctx.author.name}")
+    else:
+      embed = discord.Embed(title="You can use the command with 'a;<command name>'.",color=0x74596d) 
+      embed.timestamp = datetime.utcnow() 
+      embed.add_field(name="help",value="Show you help.",inline=False) 
+      embed.add_field(name="sinfo <曲名>",value="Get song information.",inline=False) 
+      embed.add_field(name="pinfo <パートナー名>",value="Get partner information.",inline=False) 
+      embed.add_field(name="acsong (回数)",value="Bot randomly chooses song.",inline=False) 
+      embed.add_field(name="acpartner",value="Bot randomly chooses partner.",inline=False) 
+      embed.add_field(name="set",value="Show you setting panel.",inline=False) 
+      embed.add_field(name="Other",value="Click [here](https://discord.com/api/oauth2/authorize?client_id=702587324718120991&permissions=60480&scope=bot) for bot introduction.",inline=False) 
+      embed.set_author(name="Help",icon_url=self.bot.user.avatar_url) 
+      embed.set_footer(text=f"Author : {ctx.author.name}")
     await ctx.send(embed=e,delete_after=60)
     
   @self.bot.event
@@ -38,12 +50,36 @@ class Main(commands.Cog):
     print("<Ready> 起動しました")
     
     for g in self.bot.guilds:
+      with open("guild.json", "r", encoding="utf-8") as f:
+        guilddata = json.load(f)
       if str(g.id) in guilddata: 
         lang = guilddata[str(g.id)]["lang"]
         del guilddata[str(g.id)]
       else: lang = "eng"
       data = {
         "lang":lang,
+        "song":{
+          "ignorepacks":[], 
+          "ignoresongs":[], 
+          "levels":[[], [], [], []], 
+          "side":None, 
+          "illustrators":[], 
+          "composers":[], 
+          "chart_creators":[], 
+          "notes_limit":[[0,1600], [0,1600], [0,1600], [0,1600]], 
+          "constant_limit":[[1.0,12.0], [1.0,12.0], [1.0,12.0], [1.0,12.0]]
+        } 
+        "partner":{
+          "resident":"all", 
+          "step_limit":[[0,200], [0,200]], 
+          "frag_limit":[[0,200], [0,200]], 
+          "types":[], 
+          "skills":[]
         }
+      }
+      guilddata.setdefault(str(g.id), data)
+      with open("guild.json", "w", encoding="utf-8") as f:
+        json.dump(guiktddata, f)
+    print("<Success> サーバーデータの設定が完了しました")
              
 def setup(bot): bot.add_cog(Main(bot))
