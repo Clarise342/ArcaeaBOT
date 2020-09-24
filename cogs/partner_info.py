f138a5116152b3488b384e5608fea5a2889d2ab1
Clarice <57488906+Clarise342@users.noreply.github.com>
2020-09-24 22:00:56 +0900

    Update partner_info.py

--- cogs/partner_info.py
+++ cogs/partner_info.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from datetime import datetime
 import discord, json
 
 skill_nt = nt("Skill", "name description awaken") # skill
@@ -19,22 +20,60 @@ class PartnerInfo(commands.Cog):
     
   @bot.command()
   async def pinfo(ctx, *, name=None):
+    
+    # サーバーデータの読み込み
     with open("guild.json", "r", encoding="utf-8") as f:
       guilddata = json.load(f)
+    
+    # 送信されたメッセージの削除
     await ctx.message.edit(delete_after=3.0) 
-    if name == None: name = guilddata[str(ctx.guild.id)]["dataset"][1] if guilddata[str(ctx.guild.id)]["dataset"][1] != None else None 
-    partners = [i for i in pdata if name in i.name] 
-    if len(partners) == 0: return await ctx.send("パートナーが見つかりませんでした", delete_after=5.0) 
-    partner = partners[0] 
-    e = discord.Embed(title=f"◆ パートナー名 {partner.name}",description=f"◇ タイプ {partner.type}",color=0x74596d) 
-    e.timestamp = dt.utcnow() 
-    e.add_field(name="◇ FragとStep(最小値)",value=f"{partner.frag.min} | {partner.step.min}",inline=False) 
-    e.add_field(name="◇ FragとStep(最大値)",value=f"{partner.frag.max} | {partner.step.max}",inline=False) 
-    e.add_field(name="◇ FragとStep(覚醒後)",value=f"{partner.frag.awaken} | {partner.step.awaken}",inline=False) 
-    e.add_field(name=f"◇ Skill ({partner.skill.name})",value=f"{partner.skill.description}\n(覚醒後: {partner.skill.awaken})",inline=False) 
-    e.set_author(name=" パートナーの情報 ",icon_url=bot.user.avatar_url) 
-    e.set_footer(text=f"送信者 : {ctx.author.name}") 
-    e.set_image(url=partner.image) 
-    await ctx.send(embed=e,delete_after=60)
+    
+    # 変数 'name' が指定されていない場合は…
+    if name == None: 
+      name = guilddata[str(ctx.guild.id)]["dataset"][1] if guilddata[str(ctx.guild.id)]["dataset"][1] != None else None 
+    
+    # 該当するパートナーを検索
+    results = [l for l in pdata if name in i.name] 
+    
+    # 日本語
+    if guilddata[str(ctx.guild.id)]["lang"] = "jpn":
+   
+      # 該当するパートナーが見つからない場合は…
+      if len(results) == 0:
+        return await ctx.send(embed=discord.Embed(title="パートナーが見つかりませんでした",color=0xFFDD00), delete_after=5.0) 
+    
+      # Embed 作成準備
+      partner = results[0] 
+      embed = discord.Embed(title=f"◇ パートナー名 {partner.name}",description=f"◇ タイプ {partner.type}",color=0x74596d) 
+      embed.timestamp = datetime.utcnow() 
+      embed.add_field(name="◇ FragとStep(最小値)",value=f"{partner.frag.min} | {partner.step.min}",inline=False) 
+      embed.add_field(name="◇ FragとStep(最大値)",value=f"{partner.frag.max} | {partner.step.max}",inline=False) 
+      embed.add_field(name="◇ FragとStep(覚醒後)",value=f"{partner.frag.awaken} | {partner.step.awaken}",inline=False) 
+      embed.add_field(name=f"◇ Skill ({partner.skill.name})",value=f"{partner.skill.description}\n(覚醒後: {partner.skill.awaken})",inline=False) 
+      embed.set_author(name="<Info> パートナーの情報 ",icon_url=bot.user.avatar_url) 
+      embed.set_footer(text=f"送信者 : {ctx.author.name}") 
+      embed.set_image(url=partner.image) 
+   
+    # 英語
+    if guilddata[str(ctx.guild.id)]["lang"] = "eng":
+   
+      # 該当するパートナーが見つからない場合は…
+      if len(results) == 0:
+        return await ctx.send(embed=discord.Embed(title="No partner found.",color=0xFFDD00), delete_after=5.0) 
+    
+      # Embed 作成準備
+      partner = results[0] 
+      embed = discord.Embed(title=f"◇ Partner Name {partner.name}",description=f"◇ Type {partner.type}",color=0x74596d) 
+      embed.timestamp = datetime.utcnow() 
+      embed.add_field(name="◇ Frag, Step(minimum)",value=f"{partner.frag.min} | {partner.step.min}",inline=False) 
+      embed.add_field(name="◇ Frag, Step(maximum)",value=f"{partner.frag.max} | {partner.step.max}",inline=False) 
+      embed.add_field(name="◇ Frag, Step(awakened)",value=f"{partner.frag.awaken} | {partner.step.awaken}",inline=False) 
+      embed.add_field(name=f"◇ Skill ({partner.skill.name})",value=f"{partner.skill.description}\n(覚醒後: {partner.skill.awaken})",inline=False) 
+      embed.set_author(name="<Info> Partner Information ",icon_url=bot.user.avatar_url) 
+      embed.set_footer(text=f"Author : {ctx.author.name}") 
+      embed.set_image(url=partner.image)
+  
+    # Embed 送信
+    await ctx.send(embed=embed,delete_after=60)
     
 def setup(bot): bot.add_cog(PartnerInfo(bot))
